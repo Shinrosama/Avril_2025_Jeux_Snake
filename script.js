5153c3699acbe96b9ea6f4dbf8fde042bfb5581e
Shinrosama <shinroshinn@msn.com>
2025-04-02 11:19:08 +0200

    add the allowed directions and the key codes to move the snake

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.onload = function()
         //on donne un contexte au canvas
         ctx = canvas.getContext("2d");
         // on a defini le serpent de base qui a une taille de 3 blocs
-        snakee = new snake([[6,4],[5,4],[4,4]]);
+        snakee = new snake([[6,4],[5,4],[4,4]], "right");
         refreshCanvas();
     
     }
@@ -50,9 +50,12 @@ window.onload = function()
         ctx.fillRect(x, y, blockSize, blockSize);
     }
 // on definit le comportement du corps du serpent, la couleur, le mouvement 
-    function snake(body)
+    function snake(body, direction)
     {
         this.body = body;
+        //on definit une nouvelle propriété de directions pour le serpent
+        this.direction = direction;
+
         //on crée une methode pour dessiner le corps du serpent
         this.draw = function()
         {
@@ -74,12 +77,88 @@ window.onload = function()
         {
             //on définit la nouvelle position de la tête avec slice qui crée une copie de la tête   
             let nextPosition = this.body[0].slice();
-            //on fait ensuite avancer de 1 l'éllément sur l'axe des x
-            nextPosition[0] += 1;
+            //on crée un switch qui va analiser notre direction
+            switch(this.direction)
+            {
+                //on defini les directions posibles sur l'axe des x ([0])   
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                //on defini les directions posibles sur l'axe des x ([1])                     
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw("invalid direction");
+
+                
+
+            }
             //on rajoute au debut du corps du serpent la nouvelle position (le serpent est plus long d'un élément)
             this.body.unshift(nextPosition);
             // on retire le dernier ellémént du tableau ( pour conserver la taille du serpent)
             this.body.pop();
         }
+        //on crée une nouvelle méthode pour donner au serpent sa direction (toute direction n'est pas permise en fonction de la postion du serpent)
+        this.setDirection = function(newDirection)
+        {
+            //on crée une variable pour les directions permises
+            let alloweDirections;
+            //on crée un switch sur la direction actuelle, pour definir les directions permises
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    alloweDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    alloweDirections = ["left", "right"];
+                    break;
+                //si aucune de ces situations ne se produit alors on renvoi le message suivant    
+                default:
+                    throw("invalid direction");
+            }
+            //on veux permettre le changement de direction si celle ci est permise
+            //le idex of se base sur l'index des tableau de direction 
+            if(alloweDirections.indexOf(newDirection) > -1)
+            {
+                this.direction = newDirection;
+            }
+        }
+    }
+//on veu créer un évenement quand l'utilisateur appuie sur une touche de son clavier
+    document.onkeydown = function handleKeyDown(e)
+    {
+        //chaque touche appuyée a un code, ce code est donné par l'évenement (e)
+        let key = e.keyCode;
+        //ensuite on veu donner une direction au serpent en fonction de la touche appuyée
+        let newDirection;
+        switch(key)
+        {
+            // le chiffre est le code de la direction
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            //si aucune des situations précédentes ne se produit alors on reviens au début
+            default:
+                return;
+        }
+        snakee.setDirection(newDirection);
     }
 }
