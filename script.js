054268ab550d6ccdcd69ef473f65c0d7218be7f9
Shinrosama <shinroshinn@msn.com>
2025-04-10 11:22:08 +0200

    add style for the project and corected bug by implementing timeout when refreshing game whith spacebar

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ window.onload = function()
     let widthInBlocks = canvasWidth/blockSize;
     let heightInBlocks = canvasHeight/blockSize;
     let score;
+    let timeOut;
 //on appelle la fonction init
     init();
 // on crée la fonction init qui définit l'état de base de la page
-    function init()
-    {
+    function init(){
         //on crée le canvas dans le projet
         let canvas = document.createElement('canvas');
         // on lui attribut ses dimensions
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
         //on crée la bordure de l'espace de jeux
-        canvas.style.border = "1px solid";
+        canvas.style.border = "30px solid gray";
+        canvas.style.margin = "50px auto"
+        canvas.style.display = "block"
+        canvas.style.backgroundColor = "#ddd"
         //on place le canvas dans le body de la page
         document.body.appendChild(canvas);
         //on donne un contexte au canvas
@@ -40,8 +43,7 @@ window.onload = function()
     
     }
 // on définit la fonction qui met a jours la progression du jeux 
-    function refreshCanvas()
-    {
+    function refreshCanvas(){
         //on fait avancer le serpent    
         snakee.advance();
 
@@ -52,49 +54,64 @@ window.onload = function()
         }
         else
         {
-            if(snakee.isEatingApple(applee))
-            {
+            if(snakee.isEatingApple(applee)){
                 score++;
                 snakee.ateApple = true;
-                do
-                {
+                do {
                    applee.setNewPosition();
                 }
                 while(applee.isOnSnake(snakee)) 
             }
             ctx.clearRect(0,0,canvasWidth, canvasHeight);
+            drawScore();
             snakee.draw();
             applee.draw();
-            drawScore();
-            setTimeout(refreshCanvas, delay);
+            
+            timeOut = setTimeout(refreshCanvas, delay);
         }
 
     }
-    function gameOver()
-    {
+    function gameOver(){
         ctx.save();
-        ctx.fillText("Game Over", 5, 15);
-        ctx.fillText("Appuyez sur la touche espace pour rejouer", 5, 30);
+        ctx.font = "bold 70px sans-serif";
+        ctx.fillStyle = "#000";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 5;
+        let centerX = canvasWidth / 2;
+        let centerY = canvasHeight / 2;
+        ctx.strokeText("Game Over", centerX, centerY - 180);
+        ctx.fillText("Game Over", centerX, centerY - 180);
+        ctx.font = "bold 30px sans-serif";
+        ctx.strokeText("Appuyez sur la touche espace pour rejouer", centerX, centerY - 120);
+        ctx.fillText("Appuyez sur la touche espace pour rejouer", centerX, centerY - 120 );
         ctx.restore();
     }
 
-    function restart()
-    { 
+    function restart(){ 
         snakee = new snake([[6,4],[5,4],[4,4]], "right");
         applee = new apple([10, 10]);
         score = 0;
+        clearTimeout(timeOut)
         refreshCanvas();
     }
 
-    function drawScore()
-    {
+    function drawScore(){
         ctx.save();
-        ctx.fillText(score.toString(), 5, canvasHeight -5);
+        //on donne du style a notre score
+        ctx.font = "bold 200px sans-serif";
+        ctx.fillStyle = "gray";
+        ctx.textAlign = "center";
+        //on corige un décalage en placant le texte par rapport au millieu
+        ctx.textBaseline = "middle"
+        let centerX = canvasWidth / 2;
+        let centerY = canvasHeight / 2;
+        ctx.fillText(score.toString(), centerX, centerY);
         ctx.restore();
     }
 // on crée une fontion pris prend en compte le contexte et la position d'un bloc (corps du serpent)eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    function drawBlock(ctx, position)
-    {
+    function drawBlock(ctx, position){
         //en pixels on prends la position (dans le tableau) qu'on multiplie par la taille du bloc
         let x = position[0] * blockSize;
         let y = position[1] * blockSize;
@@ -102,8 +119,7 @@ window.onload = function()
         ctx.fillRect(x, y, blockSize, blockSize);
     }
 // on definit le comportement du corps du serpent, la couleur, le mouvement 
-    function snake(body, direction)
-    {
+    function snake(body, direction){
         this.body = body;
         //on definit une nouvelle propriété de directions pour le serpent
         this.direction = direction;
@@ -117,8 +133,7 @@ window.onload = function()
             //on donne sa couleur au serpent
             ctx.fillStyle = "#1C1C1C";
             //pour dessiner le serpent on crée une boucle
-            for(let i = 0; i < this.body.length; i++)
-            {
+            for(let i = 0; i < this.body.length; i++){
                 //on crée une fonction qui crée un bloc
                 drawBlock(ctx, this.body[i]);
             }
@@ -126,13 +141,11 @@ window.onload = function()
             ctx.restore();
         }
 //on crée une fontion pour faire avancer le serpent
-        this.advance = function()
-        {
+        this.advance = function(){
             //on définit la nouvelle position de la tête avec slice qui crée une copie de la tête   
             let nextPosition = this.body[0].slice();
             //on crée un switch qui va analiser notre direction
-            switch(this.direction)
-            {
+            switch(this.direction){
                 //on defini les directions posibles sur l'axe des x ([0])   
                 case "left":
                     nextPosition[0] -= 1;
@@ -153,24 +166,19 @@ window.onload = function()
             //on rajoute au debut du corps du serpent la nouvelle position (le serpent est plus long d'un élément)
             this.body.unshift(nextPosition);
 
-            if (!this.ateApple)
-            {
+            if (!this.ateApple){
               // on retire le dernier ellémént du tableau ( pour conserver la taille du serpent)
                 this.body.pop();   
-            }
-            else
-            {
+            } else {
                 this.ateApple = false;    
             }
         }
         //on crée une nouvelle méthode pour donner au serpent sa direction (toute direction n'est pas permise en fonction de la postion du serpent)
-        this.setDirection = function(newDirection)
-        {
+        this.setDirection = function(newDirection){
             //on crée une variable pour les directions permises
             let alloweDirections;
             //on crée un switch sur la direction actuelle, pour definir les directions permises
-            switch(this.direction)
-            {
+            switch(this.direction){
                 case "left":
                 case "right":
                     alloweDirections = ["up", "down"];
@@ -185,14 +193,12 @@ window.onload = function()
             }
             //on veux permettre le changement de direction si celle ci est permise
             //le idex of se base sur l'index des tableau de direction 
-            if(alloweDirections.indexOf(newDirection) > -1)
-            {
+            if(alloweDirections.indexOf(newDirection) > -1){
                 this.direction = newDirection;
             }
         }
         //on crée une fonction comme une methode pour verifier les colision
-        this.checkColision = function()
-        {
+        this.checkColision = function(){
             //on détermine la colision avec les murs
             let wallColision = false;
             //on détermine la colision avec le corps du serpent
@@ -213,14 +219,12 @@ window.onload = function()
             let isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
             let isNotBetweenHorVerticalWalls = snakeY < minY || snakeY > maxY;
             // on détermine les conditions pour savoir si il y a colision
-            if(isNotBetweenHorizontalWalls || isNotBetweenHorVerticalWalls)
-            {
+            if(isNotBetweenHorizontalWalls || isNotBetweenHorVerticalWalls){
                 wallColision = true;
             }
             /*pour verifier les cilisions de la tête avec le corps, on verifie si les coordonnées 
             de la tête correspondent a celles d'un des éléments du reste du corp*/
-            for(let i = 0; i < rest.length; i++)
-            {
+            for(let i = 0; i < rest.length; i++){
                 if(snakeX === rest[i][0] && snakeY === rest[i][1])
                 {
                    snakeColision = true; 
@@ -229,8 +233,7 @@ window.onload = function()
             return wallColision || snakeColision;
         };
         // on verifi si le serpent est entrain de manger une pomme, on donne en argument les coordonées d'une pomme
-        this.isEatingApple = function(appleToEat)
-        {
+        this.isEatingApple = function(appleToEat){
             //on definit la tete dont la position est egale au corps a la position 0
             let head = this.body[0];
             //on verifie que les coordonées x et y de la tête du serpent sent egales aux coordonées x et y de la pomme
@@ -242,11 +245,9 @@ window.onload = function()
     }
 
     //on veu créer la pomme
-    function apple(position)
-    {
+    function apple(position){
         this.position = position;
-        this.draw = function()
-        {
+        this.draw = function(){
             //on sauvegarde la configuration actuelle du contexte
             ctx.save();
             //on donne la couleur a utiliser
@@ -265,8 +266,7 @@ window.onload = function()
             // on restaure le contexte précédemment sauvé
             ctx.restore();
         }
-        this.setNewPosition = function()
-        {
+        this.setNewPosition = function(){
             /*on utilise math.random pour obtenir un nombre aléatoire entre 0 et 1
             les valeurs du random n'étant pas forcement des nombre entier on utilise Math.round pour arondir
             on multiplie le résultat par le nombre de blocs -1
@@ -276,14 +276,11 @@ window.onload = function()
             this.position = [newX, newY]
         }
         //on veut eviter que la pomme apparaisse sur le serpent
-        this.isOnSnake = function(snakeToCheck)
-        {
+        this.isOnSnake = function(snakeToCheck){
             let isOnSnake = false;
             //on verifie si la pomme est sur un bloc du serpent
-            for(let i = 0; i < snakeToCheck.body.length; i++)
-            {
-                if(this.position[0] === snakeToCheck.body[i][0] && this.position[1] === snakeToCheck.body[i][1])
-                {
+            for(let i = 0; i < snakeToCheck.body.length; i++){
+                if(this.position[0] === snakeToCheck.body[i][0] && this.position[1] === snakeToCheck.body[i][1]){
                     isOnSnake = true;
                 }
             }
@@ -291,14 +288,12 @@ window.onload = function()
         }
     }
 //on veu créer un évenement quand l'utilisateur appuie sur une touche de son clavier
-    document.onkeydown = function handleKeyDown(e)
-    {
+    document.onkeydown = function handleKeyDown(e){
         //chaque touche appuyée a un code, ce code est donné par l'évenement (e)
         let key = e.keyCode;
         //ensuite on veu donner une direction au serpent en fonction de la touche appuyée
         let newDirection;
-        switch(key)
-        {
+        switch(key){
             // le chiffre est le code de la direction
             case 37:
                 newDirection = "left";
